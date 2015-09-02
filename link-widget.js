3fe7ba923a35c743a416e6e050bf592455871be7
Thomas Marstrander <thomasmars87@gmail.com>
2015-09-02 16:23:30 +0200

    Capitalized title and class. Moved errors to separate error field. Validate trimmed url value. [HFJ-908]

--- link-widget.js
+++ link-widget.js
@@ -1,5 +1,5 @@
 /*global H5PEditor, H5P */
-H5PEditor.widgets.linkWidget = (function ($) {
+H5PEditor.widgets.LinkWidget = (function ($) {
 
   /**
    * Initialize link widget
@@ -9,7 +9,7 @@ H5PEditor.widgets.linkWidget = (function ($) {
    * @param params
    * @param setValue
    */
-  function linkWidget(parent, field, params, setValue) {
+  function LinkWidget(parent, field, params, setValue) {
     var self = this;
 
     // Tell editor to handle passing readies.
@@ -20,20 +20,34 @@ H5PEditor.widgets.linkWidget = (function ($) {
       'class': 'h5p-link-widget'
     });
 
+    // Link fields
+    var $linkFields = $('<div>', {
+      'class': 'h5p-link-fields'
+    }).appendTo($container);
+
+    // Error field
+    var $errorField = $('<div>', {
+      'class': 'h5p-link-errors'
+    }).appendTo($container);
+
     // Extend params with default values
     params = $.extend({}, params);
     setValue(field, params);
 
     // Process semantics and place them in container
-    H5PEditor.processSemanticsChunk(field.fields, params, $container, self);
+    H5PEditor.processSemanticsChunk(field.fields, params, $linkFields, self);
 
     // Selector element
-    var $selector = $container.find('.field.select').addClass('h5p-link-protocol-selector')
-      .find('select');
+    var $selectElement = $linkFields.find('.field.select').addClass('h5p-link-protocol-selector');
+    var $selector = $selectElement.find('select');
 
     // Url text element
-    var $urlText = $container.find('.field.text').addClass('h5p-link-url')
-      .find('.h5peditor-text');
+    var $urlElement = $linkFields.find('.field.text').addClass('h5p-link-url');
+    var $urlText = $urlElement.find('.h5peditor-text');
+
+    // Move error messages
+    $selectElement.find('.h5p-errors').appendTo($errorField);
+    $urlElement.find('.h5p-errors').appendTo($errorField);
 
     // Register listener for changes in url field
     $urlText.on('input propertychange paste', function () {
@@ -63,7 +77,7 @@ H5PEditor.widgets.linkWidget = (function ($) {
     self.validate = function () {
 
       // We only require the URL field to be non-empty
-      return $urlText.val().length >= 1;
+      return $urlText.val().trim().length >= 1;
     };
 
     /**
@@ -83,6 +97,6 @@ H5PEditor.widgets.linkWidget = (function ($) {
     };
   }
 
-  return linkWidget;
+  return LinkWidget;
 
 })(H5P.jQuery);
